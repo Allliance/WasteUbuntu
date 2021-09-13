ba49bec12695d5b2c53c1aa14bee1d3ea1a6bba1
allliance <alians310322@gmail.com>
2021-09-13 14:30:03 +0430

    Improve post page

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 
 print("hello guys")
 app = Flask(__name__)
 
+
 @app.route('/')
-def indexd():
+def index():
     return render_template('index.html')
+
+
+@app.route('/post', methods=['POST', 'GET'])
+def post():
+    return render_template('post.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
